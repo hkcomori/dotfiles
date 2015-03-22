6ada5066a31c7ce5225cc7ba5f4e73992700f8e5
hkcomori <hkcomori@gmail.com>
2015-03-22 21:40:01 +0900

    Diredで今日更新されたファイルを色分けする設定を追加

--- .emacs.d/conf/30-extension.el
+++ .emacs.d/conf/30-extension.el
@@ -247,6 +247,30 @@ do nothing. And suppress the output from `message' and
 																 (looking-at " "))
 								 dired-marker-char ?\040)))
 				(dired-mark arg)))
+		;; 更新日が今日であるファイルを色分けする
+		(defface dired-todays-face '((t (:foreground "blue"))) nil)
+		(defvar dired-todays-face 'dired-todays-face)
+		(defconst month-name-alist
+			'(("1"  . "Jan") ("2"  . "Feb") ("3"  . "Mar") ("4"  . "Apr")
+				("5"  . "May") ("6"  . "Jun") ("7"  . "Jul") ("8"  . "Aug")
+				("9"  . "Sep") ("10" . "Oct") ("11" . "Nov") ("12" . "Dec")))
+		(defun dired-today-search (arg)
+			"Fontlock search function for dired."
+			(search-forward-regexp
+			 (let ((month-name
+							(cdr (assoc (format-time-string "%b") month-name-alist))))
+				 (if month-name
+						 (format
+							(format-time-string
+							 "\\(%Y-%m-%d\\|%b %e\\|%%s %e\\) [0-9]....") month-name)
+					 (format-time-string
+						"\\(%Y-%m-%d\\|%b %e\\) [0-9]....")))
+			 arg t))
+		(eval-after-load "dired"
+			'(font-lock-add-keywords
+				'dired-mode
+				(list '(dired-today-search . dired-todays-face))))
+		;; Diredの設定
 		(setq dired-dwim-target t)							;左右分割時に他方を転送先にする
 		(setq dired-recursive-copies  'always)	;常に再帰コピー
 		(setq dired-recursive-deletes 'always)	;常に再帰削除
