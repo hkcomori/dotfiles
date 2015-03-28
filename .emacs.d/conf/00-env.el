0d026a92f0fdcdd383f7692894fda2e9e6eb3535
hkcomori <hkcomori@gmail.com>
2015-03-28 19:40:59 +0900

    バイトコンパイル関数を変更

--- .emacs.d/conf/00-env.el
+++ .emacs.d/conf/00-env.el
@@ -63,11 +63,19 @@
 
 ;; カレントバッファのファイルをバイトコンパイル
 ;; http://ergoemacs.org/emacs/emacs_byte_compile.html
-(defun byte-compile-current-buffer ()
+(defun byte-recompile-current-buffer ()
 	"`byte-compile' current buffer if it's emacs-lisp-mode and compiled file exists."
 	(interactive)
 	(when (and (eq major-mode 'emacs-lisp-mode)
 						 (file-exists-p (byte-compile-dest-file buffer-file-name)))
+		(byte-compile-file buffer-file-name)))
+
+;; カレントバッファのファイルをバイトコンパイル
+;; http://ergoemacs.org/emacs/emacs_byte_compile.html
+(defun byte-compile-current-buffer ()
+	"`byte-compile' current buffer if it's emacs-lisp-mode."
+	(interactive)
+	(when (eq major-mode 'emacs-lisp-mode)
 		(byte-compile-file buffer-file-name)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
