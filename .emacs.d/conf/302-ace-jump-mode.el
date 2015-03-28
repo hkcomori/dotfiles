8e52a8625153ae5ec60f0c7d7037edf9af1d2f3c
hkcomori <hkcomori@gmail.com>
2015-03-28 19:41:21 +0900

    パッケージの追加

---
+++ .emacs.d/conf/302-ace-jump-mode.el
@@ -0,0 +1,6 @@
+(require 'ace-jump-mode)
+(setq ace-jump-mode-gray-background nil)
+(setq ace-jump-word-mode-use-query-char nil)
+(setq ace-jump-mode-move-keys
+			(append "asdfghjkl;:]qwertyuiop@zxcvbnm,." nil))
+(global-set-key (kbd "C-:") 'ace-jump-word-mode)
