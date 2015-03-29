fe66c2826005f795ad32169d7c623f48a9314cdf
hkcomori <hkcomori@gmail.com>
2015-03-29 21:06:18 +0900

    color-moccurの設定ファイルを追加

---
+++ .emacs.d/conf/326-color-moccur.el
@@ -0,0 +1,16 @@
+(require 'color-moccur)
+(require 'moccur-edit)
+
+(setq moccur-split-word t)							;スペース区切りでAND検索
+
+;; moccur-edit-finish-editと同時にファイルを保存する
+(defadvice moccur-edit-change-file
+	(after save-after-moccur-edit-buffer activate)
+	(save-buffer))
+
+(define-key global-map (kbd "M-o")	'occur-by-moccur)
+(define-key global-map (kbd "M-g")	'moccur-grep-find)
+
+(when (and (executable-find "cmigemo")
+					 (require 'migemo nil t))
+	(setq moccur-use-migemo t))
