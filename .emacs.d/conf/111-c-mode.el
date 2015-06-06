62ae960c477f7af4b24df871c81cb77b57b4a750
hkcomori <hkcomori@gmail.com>
2015-06-06 17:57:49 +0900

    C言語で割込禁止/許可を太字で色分けするように変更

--- .emacs.d/conf/111-c-mode.el
+++ .emacs.d/conf/111-c-mode.el
@@ -1,9 +1,12 @@
 ;;; Code:
 ;; Syntaxハイライト設定
+(defface font-lock-c-di-face '((t (:foreground "blue" :bold t))) nil)
+(defface font-lock-c-ei-face '((t (:foreground "red" :bold t))) nil)
 (font-lock-add-keywords
  'c-mode '(
 					 ("\\<m?i_[a-zA-Z0-9]+\\>" . 'font-lock-variable-name-face)
-					 ("\\<Intcrl\\(Enable\\|Disable\\)AllInterrupt\\>()" . 'font-lock-keyword-face)
+					 ("\\<IntcrlDisableAllInterrupt\\>()" . 'font-lock-c-di-face)
+					 ("\\<IntcrlEnableAllInterrupt\\>()" . 'font-lock-c-ei-face)
 					 ("\\<MYSTATIC\\>" . 'font-lock-keyword-face)
 					 ("\\<\\(ST\\|EM\\)_[a-zA-Z0-9_]+\\>" . 'font-lock-type-face)
 					 ("\\<[A-Z][A-Z0-9_]+[A-Z0-9]\\>" . 'font-lock-constant-face)
