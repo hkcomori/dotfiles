e118138e44728bf435790f4625a5a1f008ca12e2
hkcomori <hkcomori@gmail.com>
2021-06-10 15:45:42 +0900

    Delete unused files

--- .emacs.d/conf/340-volatile-highlights.el
+++
@@ -1,9 +0,0 @@
-;;; Code:
-(require 'volatile-highlights)
-(volatile-highlights-mode)
-
-;; Local Variables:
-;; coding: utf-8
-;; End:
-
-;;; 340-volatile-highlights.el ends here
--- .emacs.d/init.el
+++
@@ -1,9 +0,0 @@
-;-*- coding: utf-8; -*-
-;;--------------------------------------------------------------
-;; Emacs init script
-;;     written by thyobu create at 2013-12-17
-;;--------------------------------------------------------------
-(load-file "~/.emacs.d/conf/000-env.el")
-
-(require 'init-loader)
-(init-loader-load "~/.emacs.d/conf")
