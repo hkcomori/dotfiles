0190f609ae61c9caf097918b150962ac7bae3c7d
hkcomori <hkcomori@gmail.com>
2022-09-14 10:27:27 +0900

    Add peco config

--- setup.d/00-peco.sh
+++ setup.d/00-peco.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env -S bash -e -o pipefail
 
+. "$(dirname "$0")"/lib.sh
+
 umask 022
 export DEBIAN_FRONTEND=noninteractive
 
+ensure_link \
+    .peco/config.json
+
 sudo apt-get install -y \
-    cloc \
-    jq
+    peco
