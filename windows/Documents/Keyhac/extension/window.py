76f97c75ca482afec7ccc525b8d2394f0c2129ae
hkcomori <hkcomori@gmail.com>
2023-06-24 21:59:01 +0900

    [keyhac] Add Window.set_foreground

--- windows/Documents/Keyhac/extension/window.py
+++ windows/Documents/Keyhac/extension/window.py
@@ -1,18 +1,14 @@
 from ctypes import (
     windll,
     pointer,
-    c_bool,
 )
 from ctypes.wintypes import (
     POINT,
     HWND,
+    DWORD,
 )
 import enum
 from logging import getLogger
-from time import sleep
-
-
-import pyauto
 
 
 RETRY_COUNT = 3
@@ -30,19 +26,7 @@ class IMEWindowNotFoundError(WindowNotFoundError):
     pass
 
 
-def restype_bool(value) -> bool:
-    if value == 0:
-        return True
-    return False
-
-
 user32 = windll.user32
-user32.IsWindow.restype = c_bool
-user32.SetForegroundWindow.restype = c_bool
-user32.GetForegroundWindow.restype = HWND
-user32.GetWindow.restype = HWND
-user32.WindowFromPoint.restype = HWND
-
 imm32 = windll.imm32
 
 
@@ -65,52 +49,53 @@ class Window:
         self._hwnd = hwnd
         if not user32.IsWindow(self._hwnd):
             raise WindowNotFoundError(f'hwnd={self._hwnd}')
-        self._window = pyauto.Window.fromHWND(self._hwnd)
+        self._thread = Thread(self._hwnd)
 
-    def __equal__(self, other: 'Window') -> bool:
+    def __eq__(self, other: 'Window') -> bool:
         return self._hwnd == other._hwnd
 
-    @property
-    def title(self) -> str:
-        return self._window.getText()
+    def __str__(self) -> str:
+        return str(self._hwnd)
 
-    @property
-    def process_name(self) -> str:
-        return self._window.getProcessName()
-
-    def _set_foreground(self, force: bool = False):
-        self._window.setForeground(force)
+    def _set_foreground(self, current: 'Window') -> 'Window':
+        user32.SetForegroundWindow(self._hwnd)
+        new_hwnd = current._hwnd
+        while new_hwnd == current._hwnd:
+            new_hwnd: HWND = user32.GetForegroundWindow()
+        if new_hwnd == self._hwnd:
+            return self
+        if (new_hwnd != current._hwnd) and (self._hwnd == user32.GetWindow(new_hwnd, Window.Relation.OWNER)):
+            return self.__class__.from_hwnd(new_hwnd)
+        raise RuntimeError(f'SetForegroundWindow failure: target={self}, current={current}, new={new_hwnd}')
 
     def set_foreground(self) -> 'Window':
-        # _target = pyauto.Window.fromHWND(self._hwnd)
-        # current_window = self.__class__.from_foreground()
-        # if current_window == self:
-        #     return self
+        self_thread = Thread(self)
+        old_fore = self.__class__.from_foreground()
+        old_thread = Thread(old_fore)
 
-        for i in range(0, RETRY_COUNT):
-            self._set_foreground(True)
-            current = self.__class__.from_foreground()
-            result = self == current
-            # result: bool = user32.SetForegroundWindow(self._hwnd)
-            # sleep(RETRY_WAIT_MS)
-            if not result:
-                t_text = self.process_name
-                c_text = current.process_name
-                logger.debug(f'SetForegroundWindow failed: target={t_text}, fore={c_text}')
-                continue
-            # try:
-            # new_window = self.__class__.from_foreground()
-            # except WindowNotFoundError:
-            #     continue
-            # if new_window != current_window:
-            #     break
+        if user32.IsIconic(self._hwnd):
+            SW_RESTORE = 9
+            user32.ShowWindow(self._hwnd, SW_RESTORE)
 
-        return self
-        # if new_window == self:
-        #     return self
-        # if new_window.get_owner() == self:
-        #     return new_window
-        # raise WindowNotFoundError(f'hwnd={self._hwnd}')
+        if old_fore == self:
+            logger.debug(f'Foreground: (hwnd={old_fore}, thread={old_thread}) => (hwnd={self}, thread={self_thread}), Same hwnd')
+            return old_fore
+        if old_thread == self_thread:
+            logger.debug(f'Foreground: (hwnd={old_fore}, thread={old_thread}) => (hwnd={self}, thread={self_thread}), Same thread')
+            return old_fore
+
+        with self_thread.attach(old_thread):
+            new_fore = self._set_foreground(old_fore)
+            new_thread = Thread(new_fore)
+
+        user32.BringWindowToTop(self._hwnd)
+        user32.SetFocus(self._hwnd)
+
+        if (new_fore == self) or (new_thread == self_thread):
+            logger.debug(f'Foreground: (hwnd={old_fore}, thread={old_thread}) => (hwnd={self}, thread={self_thread})')
+        else:
+            logger.error(f'Foreground: (hwnd={old_fore}, thread={old_thread}) => (hwnd={new_fore}, thread={new_thread}), expected=(hwnd={self}, thread={self_thread})')
+        return new_fore
 
     def ime_on(self):
         IME(self._hwnd).status = IME.Status.ON
@@ -137,7 +122,7 @@ class Window:
     @classmethod
     def from_foreground(cls) -> 'Window':
         hwnd: HWND = user32.GetForegroundWindow()
-        if not hwnd:
+        if hwnd == 0:
             raise WindowNotFoundError('No foreground window')
         return cls(hwnd)
 
@@ -172,3 +157,30 @@ class Cursor:
         point = POINT()
         user32.GetCursorPos(pointer(point))
         return point
+
+
+class Thread:
+    def __init__(self, window: 'Window'):
+        self._thread_id: DWORD = user32.GetWindowThreadProcessId(window._hwnd, 0)
+
+    def __eq__(self, other: 'Thread') -> bool:
+        return self._thread_id == other._thread_id
+
+    def __str__(self) -> str:
+        return str(self._thread_id)
+
+    def attach(self, other: 'Thread') -> 'Thread.AttacheInput':
+        return Thread.AttacheInput(self._thread_id, other._thread_id)
+
+    class AttacheInput:
+        def __init__(self, self_thread_id, other_thread_id):
+            self._self_thread_id = self_thread_id
+            self._other_thread_id = other_thread_id
+            self._attached = False
+
+        def __enter__(self):
+            self._attached = user32.AttachThreadInput(self._self_thread_id, self._other_thread_id, True)
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            if self._attached:
+                user32.AttachThreadInput(self._self_thread_id, self._other_thread_id, False)
