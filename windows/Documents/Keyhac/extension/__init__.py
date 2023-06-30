5556f3948bb893aa5e67351f39f3060978941c59
hkcomori <hkcomori@gmail.com>
2023-06-30 17:07:31 +0900

    [keyhac] Refactor for keymap

--- windows/Documents/Keyhac/extension/__init__.py
+++ windows/Documents/Keyhac/extension/__init__.py
@@ -1,21 +1,7 @@
-from .keymap import (
-    KeymapDefinition,
-    KeyCondition,
-)
+import keyhac
+
+from . import keymap
 
 
-def init(keymap):
-    keymap.defineModifier(KeyCondition("Apps").to_keyhac(), "User0")
-    keymap.defineModifier(KeyCondition("Kana").to_keyhac(), "User1")
-    keymap.defineModifier(KeyCondition("Henkan").to_keyhac(), "User2")
-    keymap.defineModifier(KeyCondition("Muhenkan").to_keyhac(), "User3")
-
-    for keymap_definition in KeymapDefinition.all():
-        window_keymap = keymap.defineWindowKeymap(
-            keymap_definition.exe_name,
-            keymap_definition.class_name,
-            keymap_definition.window_text,
-            keymap_definition.check_func,
-        )
-        for k, v in keymap_definition.items():
-            window_keymap[k] = v
+def init(_keymap: keyhac.Keymap):
+    keymap.init(_keymap)
--- windows/Documents/Keyhac/extension/keymap/__init__.py
+++ windows/Documents/Keyhac/extension/keymap/__init__.py
@@ -1,7 +1,27 @@
+import keyhac
+
 from .common import (    # noqa: F401
     KeymapDefinition,
     KeyCondition,
+    WindowKeymap,
 )
 from . import (          # noqa: F401
     def_global,
 )
+
+
+def init(keymap: keyhac.Keymap):
+    keymap.defineModifier(KeyCondition("Apps").to_keyhac(), "User0")
+    keymap.defineModifier(KeyCondition("Kana").to_keyhac(), "User1")
+    keymap.defineModifier(KeyCondition("Henkan").to_keyhac(), "User2")
+    keymap.defineModifier(KeyCondition("Muhenkan").to_keyhac(), "User3")
+
+    for keymap_definition in KeymapDefinition.all():
+        window_keymap: WindowKeymap = keymap.defineWindowKeymap(
+            keymap_definition.exe_name,
+            keymap_definition.class_name,
+            keymap_definition.window_text,
+            keymap_definition.check_func,
+        )
+        for k, v in keymap_definition.items():
+            window_keymap[k] = v
