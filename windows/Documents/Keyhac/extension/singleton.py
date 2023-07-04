e05a67495a1787abee82891e5da4cf4c5ca3bed1
hkcomori <hkcomori@gmail.com>
2023-07-04 18:22:23 +0900

    [keyhac] Add MetaSingleton

---
+++ windows/Documents/Keyhac/extension/singleton.py
@@ -0,0 +1,14 @@
+import json
+from weakref import WeakValueDictionary
+
+
+class MetaSingleton(type):
+    __instances: WeakValueDictionary = WeakValueDictionary()
+
+    def __call__(cls, *args, **kwargs):
+        dict_key = (args, json.dumps(kwargs))
+        obj = cls.__instances.get(dict_key)
+        if not obj:
+            obj = super().__call__(*args, **kwargs)
+            cls.__instances[dict_key] = obj
+        return obj
---
+++ windows/Documents/Keyhac/tests/test_singleton.py
@@ -0,0 +1,24 @@
+import pytest   # noqa: F401
+
+from extension.singleton import (    # noqa: E402
+    MetaSingleton,
+)
+
+
+class MyClass(metaclass=MetaSingleton):
+    def __init__(self, hoge):
+        self.__hoge = hoge
+
+    @property
+    def hoge(self):
+        return self.__hoge
+
+
+def test_instance_identification():
+    obj11 = MyClass(1)
+    obj12 = MyClass(1)
+    obj21 = MyClass(2)
+    assert obj11 is obj12
+    assert obj11 is not obj21
+    assert id(obj11) == id(obj12)
+    assert id(obj11) != id(obj21)
