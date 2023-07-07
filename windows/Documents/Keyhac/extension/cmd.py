9b393a4156494ed3b07cf131e9e4f59bf6cfb91d
hkcomori <hkcomori@gmail.com>
2023-07-07 14:42:57 +0900

    [keyhac] Activate window if already exist at launch_*

--- windows/Documents/Keyhac/extension/cmd.py
+++ windows/Documents/Keyhac/extension/cmd.py
@@ -8,6 +8,7 @@ from keyhac import (
 
 from .task import background_task
 from .window import (
+    WindowNotFoundError,
     Cursor,
     Window,
 )
@@ -16,19 +17,35 @@ from .window import (
 logger = getLogger(__name__)
 
 
+def launch_or_activate(process_path: str, **kwargs: str):
+    """kwargsに合致するウィンドウがないときにprocess_pathを起動する"""
+    process_name = os.path.basename(process_path)
+    find_kwargs = kwargs if kwargs else {'process_name': process_name}
+    try:
+        window = Window.from_find(**find_kwargs)
+    except WindowNotFoundError:
+        shellExecute(None, process_path)
+    else:
+        window.set_foreground()
+
+
 def launch_calc():
-    """電卓を起動する"""
+    """電卓を起動またはアクティブ化する"""
     logger.debug("launch_calc")
     calc_path = "calc.exe"
-    shellExecute(None, calc_path)
+    launch_or_activate(
+        calc_path,
+        process_name='ApplicationFrameHost.exe',
+        title='電卓'
+    )
 
 
 def launch_obsidian():
-    """Obsidianを起動する"""
+    """Obsidianを起動またはアクティブ化する"""
     logger.debug("launch_obsidian")
     userprofile = os.getenv("USERPROFILE")
     obsidian_path = f'{userprofile}/AppData/Local/Obsidian/Obsidian.exe'
-    shellExecute(None, obsidian_path)
+    launch_or_activate(obsidian_path)
 
 
 @background_task
