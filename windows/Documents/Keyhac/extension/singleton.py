4eb387fb73e4fe245ea443df270c6f4f36a34a0e
hkcomori <hkcomori@gmail.com>
2023-07-14 19:40:46 +0900

    [kayhac] Fix dict_key in MetaSingleton

--- windows/Documents/Keyhac/extension/singleton.py
+++ windows/Documents/Keyhac/extension/singleton.py
@@ -6,7 +6,7 @@ class MetaSingleton(type):
     __instances: WeakValueDictionary = WeakValueDictionary()
 
     def __call__(cls, *args, **kwargs):
-        dict_key = (args, json.dumps(kwargs))
+        dict_key = (cls, args, json.dumps(kwargs))
         obj = cls.__instances.get(dict_key)
         if not obj:
             obj = super().__call__(*args, **kwargs)
