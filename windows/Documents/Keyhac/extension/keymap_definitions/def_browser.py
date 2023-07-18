b1cd9a0dfa2b85e7ed619f2f51bfe9c905d5a008
hkcomori <hkcomori@gmail.com>
2023-07-18 20:14:04 +0900

    [keyhac] Disable IME when search box activate in browser

--- windows/Documents/Keyhac/extension/keymap_definitions/def_browser.py
+++ windows/Documents/Keyhac/extension/keymap_definitions/def_browser.py
@@ -12,6 +12,17 @@ def init(keymap: KeymapEx):
         keymap.defineWindowKeymap(exe_name='firefox.exe'),
         keymap.defineWindowKeymap(exe_name='vivaldi.exe'),
     )
+
+    # Disable IME when search box activate
+    browser['C-E'] = cmd.make('C-E', cmd.ime_off)
+    browser['C-F'] = cmd.make('C-F', cmd.ime_off)
+    browser['C-L'] = cmd.make('C-L', cmd.ime_off)
+    browser['C-T'] = cmd.make('C-T', cmd.ime_off)
+    browser['F1'] = cmd.make('F1', cmd.ime_off)
+    browser['F3'] = cmd.make('F3', cmd.ime_off)
+    browser['S-F3'] = cmd.make('S-F3', cmd.ime_off)
+    browser['C-S-P'] = cmd.make('C-S-P', cmd.ime_off)
+
     # Switch tabs by back/forward buttons
     browser['XButton1'] = cmd.send_under_mouse_pointer('C-S-Tab')
     browser['XButton2'] = cmd.send_under_mouse_pointer('C-Tab')
