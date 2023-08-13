01fa7d23e153783f74ca59e12d5ee565f5da1eb1
hkcomori <hkcomori@gmail.com>
2023-08-13 11:10:54 +0900

    [keyhac] Delete old modules

--- windows/Documents/Keyhac/tests/mock.py
+++
@@ -1,8 +0,0 @@
-import sys
-from unittest.mock import Mock
-
-sys.modules['ctypes.windll'] = Mock()
-sys.modules['ctypes.WINFUNCTYPE'] = Mock()
-sys.modules['pyauto'] = Mock()
-sys.modules['keyhac'] = Mock()
-sys.modules['keyhac_hook'] = Mock()
