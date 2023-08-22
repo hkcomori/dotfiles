5b433d763b4d45886d4dab6f0124c0e69bb5f09a
hkcomori <hkcomori@gmail.com>
2023-08-22 17:12:45 +0900

    [keyhac] Refactor AcrobatConfig

--- windows/Documents/Keyhac/extension/application/acrobat_config.py
+++ windows/Documents/Keyhac/extension/application/acrobat_config.py
@@ -6,25 +6,25 @@ class AcrobatConfig(AbstractConfig):
     def apply(self) -> None:
         act = self._action_service
 
-        obsidian = self._keymap_service.from_query(WindowQuery(
+        acrobat = self._keymap_service.from_query(WindowQuery(
             exe_name='Acrobat.exe',
             class_name='AVL_AVView',
         ))
 
         # Close Tab
-        obsidian['F22'] = act.send('C-W')
+        acrobat['F22'] = act.send('C-W')
 
         # Switch Tabs
-        obsidian['C-PageUp'] = act.send('C-S-Tab')
-        obsidian['C-PageDown'] = act.send('C-Tab')
+        acrobat['C-PageUp'] = act.send('C-S-Tab')
+        acrobat['C-PageDown'] = act.send('C-Tab')
 
         # Zoom to fit page width
-        obsidian['C-F22'] = act.send('C-0')
+        acrobat['C-F22'] = act.send('C-0')
 
         # Back / Forward
-        obsidian['XButton1'] = act.send('A-Left')
-        obsidian['XButton2'] = act.send('A-Right')
+        acrobat['XButton1'] = act.send('A-Left')
+        acrobat['XButton2'] = act.send('A-Right')
 
         # Rotate pages
-        obsidian['C-S-WheelDown'] = act.send('C-S-Minus')
-        obsidian['C-S-WheelUp'] = act.send('C-S-Semicolon')
+        acrobat['C-S-WheelDown'] = act.send('C-S-Minus')
+        acrobat['C-S-WheelUp'] = act.send('C-S-Semicolon')
