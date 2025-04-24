ac7256aa19f0068be2cdfc0fa9b622ce4ff32ccf
hkcomori <hkcomori@gmail.com>
2025-04-24 17:03:13 +0900

    Add reln

--- setup.d/00-general.sh
+++ setup.d/00-general.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S bash -eu -o pipefail
 
+. "$(dirname "$0")"/lib.sh
+
 umask 022
 export DEBIAN_FRONTEND=noninteractive
 
@@ -7,3 +9,6 @@ sudo apt-get install -y \
     cloc \
     nkf \
     jq
+
+ensure_link \
+    .local/bin/reln
