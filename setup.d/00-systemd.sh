2a0eac27f1d2786b47ab958fb378c48f4786cbc5
hkcomori <hkcomori@gmail.com>
2025-04-24 16:56:45 +0900

    Add systemd jobs: user-backup, system-backup

--- setup.d/00-systemd.sh
+++ setup.d/00-systemd.sh
@@ -3,4 +3,13 @@
 . "$(dirname "$0")"/lib.sh
 
 ensure_link \
+    .local/bin/runenv \
+    .config/systemd/user/system-backup.service \
+    .config/systemd/user/system-backup.timer \
+    .config/systemd/user/user-backup.service \
+    .config/systemd/user/user-backup.timer \
     .config/systemd/user/ssh-tunnel@.service
+
+mkdir -p \
+    "$HOME/.config/environment.d" \
+    "$HOME/.config/runenv.d"
