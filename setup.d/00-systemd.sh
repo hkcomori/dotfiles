7cf3cc348b366fba82eeaa4e109324941061cc58
hkcomori <hkcomori@gmail.com>
2025-04-25 16:47:54 +0900

    Remove systemd jobs: user-backup, system-backup

--- setup.d/00-systemd.sh
+++ setup.d/00-systemd.sh
@@ -6,10 +6,6 @@ ensure_link \
     .local/bin/runenv \
     .config/systemd/user/runenv@.service \
     .config/systemd/user/runenv-morning@.timer \
-    .config/systemd/user/system-backup.service \
-    .config/systemd/user/system-backup.timer \
-    .config/systemd/user/user-backup.service \
-    .config/systemd/user/user-backup.timer \
     .config/systemd/user/ssh-tunnel@.service
 
 ln -Fs runenv@.service $HOME/.config/systemd/user/runenv-morning@.service
